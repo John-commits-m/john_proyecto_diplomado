118072dcf2adfdbe09e9f74217c64b2608822a73
John <cafetiza@gmail.com>
2024-11-06 13:35:16 -0600

    add section habilidades

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,6 +118,28 @@
     }
   }, true)
 
+    /**
+   * Skills animation
+   */
+    let skilsContent = select('.skills-content');
+    if (skilsContent) {
+      new Waypoint({
+        element: skilsContent,
+        offset: '80%',
+        handler: function(direction) {
+          let progress = select('.progress .progress-bar', true);
+          progress.forEach((el) => {
+            el.style.width = el.getAttribute('aria-valuenow') + '%'
+          });
+        }
+      })
+    }
+      
+
+  
+
+  
+
   /**
    * Initiate Pure Counter 
    */
